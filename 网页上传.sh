a23f76a8f5cd2dd3ba7c331877083d1ff403f0fc
MoonlightGuardian <wq.work.pro@gmail.com>
2021-11-19 22:10:42 +0800

    uodate

---
+++ 网页上传.sh
@@ -0,0 +1,7 @@
+hugo --destination ./docs --buildDrafts --cleanDestinationDir
+git init
+git add .
+git commit -m "uodate"
+git branch -M main
+git remote add origin https://github.com/MoonlightGuardian/blog.git
+git push -u origin main
